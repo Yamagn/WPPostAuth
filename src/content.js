7b80a40827d2b96f30bf34151875acc40ec3d400
yamagn <syankenpon@gmail.com>
2021-04-08 11:40:34 +0900

    initial commit

---
+++ src/content.js
@@ -0,0 +1,9 @@
+// Workplace投稿ボタンクラス名
+
+// oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 pq6dq46d p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl l9j0dhe7 abiwlrkh p8dawk7l cbu4d94t taijpn5t k4urcfbm
+
+// 投稿ボタン取得
+const classNameStr = "oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 pq6dq46d p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl l9j0dhe7 abiwlrkh p8dawk7l cbu4d94t taijpn5t k4urcfbm"
+const elements = document.getElementsByClassName(classNameStr)
+const postButtonElements = Array.prototype.filter.call(elements, item => item.ariaLabel === "投稿")
+
